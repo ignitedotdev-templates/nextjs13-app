800d205776209989401dc64a12421f4ce4fdd1fe
CharlesChinedum <charleschinedum2@gmail.com>
2024-02-27 12:33:59 +0100

    update: designed UI

--- app/page.js
+++ app/page.js
@@ -1,11 +1,24 @@
+"use client";
+
 import Image from "next/image";
+import logo from "@/public/next.svg";
 
 export default function Home() {
+  const pathname = window.location.href;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="text-3xl fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Welcome to Ignite.dev NextJs 13 App
+    <main className="h-[100dvh] w-full flex flex-col gap-7 items-center justify-center">
+      <div className="">
+        <Image src={logo} alt="logo" width={400} height={400} />
+      </div>
+      <div className="flex flex-col text-center gap-14">
+        <div className="flex flex-col text-center">
+          <h2 className="text-[36px] font-semibold text-[#344054]">
+            You’ve just ignited a new Next.Js static site!
+          </h2>
+          <p className="text-[#004EEB] text-[20px]">{pathname}</p>
+        </div>
+        <p className="text-[20px] font-medium text-[#344054]">
+          Made in Nigeria with 💖
         </p>
       </div>
     </main>
